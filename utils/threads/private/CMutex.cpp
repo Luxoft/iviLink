ff801734841a348d38b32bef8759decc07f6cf39
John Yani <vanuan@gmail.com>
2012-08-26 03:05:52 +0300

    Android: Add missing pthread_mutex_timedlock implementation

--- utils/threads/private/CMutex.cpp
+++ utils/threads/private/CMutex.cpp
@@ -30,10 +30,34 @@
 
 #include <cerrno>
 #include <cstring>
+#include <pthread.h>
 
 #include "CMutex.hpp"
 
 Logger CMutex::logger = Logger::getInstance(LOG4CPLUS_TEXT("utils.threads.CMutex"));
+
+#ifdef __BIONIC__
+int pthread_mutex_timedlock (pthread_mutex_t *mutex, struct timespec *timeout)
+{
+  struct timeval timenow;
+  struct timespec sleepytime;
+  int retcode;
+  /* This is just to avoid a completely busy wait */
+  sleepytime.tv_sec = 0;
+  sleepytime.tv_nsec = 10000000; /* 10ms */
+  while ((retcode = pthread_mutex_trylock (mutex)) == EBUSY)
+  {
+    gettimeofday (&timenow, NULL);
+    if (timenow.tv_sec >= timeout->tv_sec &&
+        (timenow.tv_usec * 1000) >= timeout->tv_nsec)
+    {
+      return ETIMEDOUT;
+    }
+    nanosleep (&sleepytime, NULL);
+  }
+  return retcode;
+}
+#endif
 
 CMutex::CMutex()
 {
